e9d6564660feb00a8eda3a252363122c77e4391d
Estefane <estefanecortes@gmail.com>
2021-04-04 16:29:35 -0300

    Ajustando responsividade em tela até 480 e ajustando menu mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,50 @@
 function action() {
-  let modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal');
   modal.style.display = 'flex';
-  let blur = document.getElementById('blur');
+  const blur = document.getElementById('blur');
   blur.classList.toggle('active');
   blur.classList.toggle('color');
 }
 
 function fechar() {
-  let modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal');
   modal.style.display = 'none';
-  let blur = document.getElementById('blur');
+  const blur = document.getElementById('blur');
   blur.classList.remove('active');
 }
+
+function outSideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+  if(!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      setTimeout(()=> {html.addEventListener(userEvent, handleOutsideClick);})
+    })
+    element.setAttribute(outside,'');
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      })
+      callback();
+    }
+  }
+}
+
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+
+function openMenu() {
+  menuList.classList.add('active');
+  menuButton.classList.add('active');  
+  outSideClick(menuList,['click','touchstart'],() => {
+    menuList.classList.remove('active');
+    menuButton.classList.remove('active');  
+  });
+}
+
+menuButton.addEventListener('click', openMenu);
+
 
