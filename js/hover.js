481de434f2f7f75bc9294a0387241edc0001323d
Isael Sobral <isael.sobral@gmail.com>
2021-04-11 00:05:31 -0300

    Criando arquivo js para manipular hovers

---
+++ js/hover.js
@@ -0,0 +1,12 @@
+//Documento criado para chamar as funções abaixo, onde quando passar e retirar o mouse das imagens svgs elas sejam trocadas
+
+function passaCursor(){
+    document.getElementById("engrenagem-acesso-adm").src="./img/engrenagem-hover.svg"; 
+    //document.getElementById("facebook").src="./img/redes-sociais/facebook-hover.svg";
+    
+}
+function retiraCursor(){
+    document.getElementById("engrenagem-acesso-adm").src="./img/engrenagem.svg"; 
+    // document.getElementById("facebook").src="./img/redes-sociais/facebook.svg";
+
+}
