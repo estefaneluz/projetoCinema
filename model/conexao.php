ae04d387c9d0b43c00248c0ac1d7000f816584b4
IsaelSobral <isael.sobral@gmail.com>
2021-05-02 21:31:32 -0300

    Criando a camada model com a conexao em php

--- model/conexao.php
+++ model/conexao.php
@@ -1,15 +1,15 @@
 <?php /*iniciando o codigo php p/ criar a conexão*/
 
-    class dataBase  {/*classe de bd p/ conectar como banco*/
+    class dataBase  {/*classe  p/ conectar como banco*/
         private $userName="root";//login do banco padrão
         private $senha ="";
        
-       //private: só a classe dataBase vai ver
+       //private: só a classe dataBase vai acessar
         public $conexao; // variável p/manipular a conexao
         //public: todo mundo tem acesso
         public function ConexaoBancoDeDados(){
-            $this->conexao = null; // inicia o valor de conexão como nulo
-            try{//tenta executar oq tem dentro      //dbname seta o BD a ser utilizado  //permite a concexao como login do banco
+            $this->conexao = null; // inicia o valor da conexão como nulo
+            try{// trye - tenta executar o que tem dentro      //dbname seta o BD a ser utilizado  //permite a concexao como login do banco
                 $this->conexao=new PDO('mysql:host=localhost; dbname=Cinemail', $this->userName, $this->senha);//drive utilizado:mysql, host> onde vai estar o banco/ localhost: a maquina local onde ta o banco
                 /*vai tentar tratar o erro por meio de atribuicao de atributo*/
                 $this->conexao-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//tipo de tratamento do erro
@@ -18,7 +18,7 @@
                 //e o PDOEXCEPTION - classe generica p/tratar erros
                 echo $excecao->getMessage();//echo exibe na tela, aq querO q ele exiba por meio de getMessage a msg de erro q foi gerada
             }
-            return $this->conexao;// se der certo a concexão ele a retona
+            return $this->conexao;// retorna a conexão caso ela funcione
         }
     }   
 
