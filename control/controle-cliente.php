668256ae420049b0e574d7bc0d776915038271a4
IsaelSobral <isael.sobral@gmail.com>
2021-05-14 02:16:25 -0300

    Adicionando edição e deletar em controle de cliente

--- control/controle-cliente.php
+++ control/controle-cliente.php
@@ -7,10 +7,27 @@
      if(isset($_POST['cadastrarCliente'])){//seta
         $nome = $_POST['nomeCliente'];
         $cpf = $_POST['cpfCliente'];
-        $idade = $_POST['idadeCliente'];
-        $sexo = $_POST['sexoCliente'];
+        $idade = $_POST['idadeCliente'];        
         $dataNascimento = $_POST['dataNascimento'];
-        if($objCliente->cadastrarCliente($nome,$cpf,$idade,$sexo, $dataNascimento)){
+        if($objCliente->cadastrarCliente($nome,$cpf,$idade,$dataNascimento)){
+            $objCliente->redirect('../acesso-adm.php');
+        }
+    }
+    /*EDITAR CLIENTE*/
+    if(isset($_POST['editarCliente'])){
+        $id =$_POST['editarCliente'];
+        $nome = $_POST['nomeCliente'];
+        $cpf = $_POST['cpfCliente'];
+        $idade = $_POST['idadeCliente'];
+        $dataNascimento = $_POST['dataNascimento'];
+        if ($objCliente->editarCliente($nome,$cpf,$idade,$dataNascimento,$id)){
+            $objCliente->redirect('../acesso-adm.php');
+        }
+    }
+    /*DELETAR CLIENTE*/
+    if(isset($_POST['deletarCliente'])){
+        $idCliente = $_POST['deletarCliente'];
+        if($objCliente->deletarCliente($idCliente)){
             $objCliente->redirect('../acesso-adm.php');
         }
     }
