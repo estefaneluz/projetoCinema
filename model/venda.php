3b31ca37525efc06be8543a091c776544bdb81f9
estefaneluz <estefanecortes@gmail.com>
2021-05-21 17:40:01 -0300

    criando funções

--- model/venda.php
+++ model/venda.php
@@ -18,57 +18,94 @@
 
         public function cadastrarVenda($cliente, $sessao, $data, $qtdIngrInt, $qtdIngrMeia){
             try{
-                $sql= 'INSERT INTO venda(id_cliente, id_func, id_sessao, data, qtdIngrInt, qtdIngrMeia, valorTotal)
-                VALUES (:id_cliente, :id_func, :id_sessao, :data, :qtdIngrInt, :qtdIngrMeia, :valorTotal)';
-
-                //pegando o id do cliente
-                $sqlCliente = "SELECT id FROM cliente WHERE cpf = $cliente";
-                $obj = new Venda();
-                $stmtCliente = $obj->runQuery($sqlCliente);
-                $stmtCliente->execute();
-                $cliente = $stmtCliente->fetch(PDO::FETCH_ASSOC);
-
-                //pegando o preço para calcular o total 
-                $sqlPreco = "SELECT id_ingresso FROM sessao WHERE id = $sessao";
-                $stmtPreco = $obj->runQuery($sqlPreco);
-                $stmtPreco->execute();
-                $preco = $stmtPreco->fetch(PDO::FETCH_ASSOC);
-                $preco = $preco['id_ingresso'];
+                $qtdAssentos = $this->getQtdAssentos($sessao);
+                $ingressosVendidos = $this->getIngressosVendidos($sessao);
+                $ingressosVendidos+= ($qtdIngrInt + $qtdIngrMeia);
                 
-                //calculando total
-                $sqlValor = "SELECT valor, meia FROM ingresso WHERE id = $preco";
-                $stmtValor = $obj->runQuery($sqlValor);
-                $stmtValor->execute();
-                $valor = $stmtValor->fetch(PDO::FETCH_ASSOC);
-                $valorTotal = ($valor['valor']*$qtdIngrInt)+($valor['meia']*$qtdIngrMeia);
+                if($ingressosVendidos<=$qtdAssentos){
+                    if($ingressosVendidos)
+                    $sql= 'INSERT INTO venda(id_cliente, id_func, id_sessao, data, qtdIngrInt, qtdIngrMeia, valorTotal)
+                    VALUES (:id_cliente, :id_func, :id_sessao, :data, :qtdIngrInt, :qtdIngrMeia, :valorTotal)';
+                    
+                    $cliente = $this->getNomeCliente($cliente);
+                    $valorTotal = $this->calcularTotal($sessao, $qtdIngrInt, $qtdIngrMeia);
 
-                $funcionario = $_SESSION['idFuncionario']; 
-                $stmt=$this->conn->prepare($sql);
-                $stmt->bindParam(":id_cliente",$cliente['id']);
-                $stmt->bindParam(":id_func",$funcionario);   
-                $stmt->bindParam(":id_sessao",$sessao);                             
-                $stmt->bindParam(":data",$data);
-                $stmt->bindParam(":qtdIngrInt",$qtdIngrInt);
-                $stmt->bindParam(":qtdIngrMeia",$qtdIngrMeia); 
-                $stmt->bindParam(":valorTotal",$valorTotal);                
-               
-                $stmt->execute();
-                /*muda status da sessao na hora da compra
-                if($stmt->rowCount()>0){
-                    $this->mudarStatus($sessao);
+                    $funcionario = $_SESSION['idFuncionario']; 
+                    $stmt=$this->conn->prepare($sql);
+                    $stmt->bindParam(":id_cliente",$cliente);
+                    $stmt->bindParam(":id_func",$funcionario);   
+                    $stmt->bindParam(":id_sessao",$sessao);                             
+                    $stmt->bindParam(":data",$data);
+                    $stmt->bindParam(":qtdIngrInt",$qtdIngrInt);
+                    $stmt->bindParam(":qtdIngrMeia",$qtdIngrMeia); 
+                    $stmt->bindParam(":valorTotal",$valorTotal);                
+                    $stmt->execute();
+                    /*muda status da sessao na hora da compra
+                    if($stmt->rowCount()>0){
+                        $this->mudarStatus($sessao);
+                        return $stmt;
+                    }*/
                     return $stmt;
-                }*/
-                return $stmt;
+                }
             }catch(PDOException $e){
                 echo("Error: ".$e->getMessage());
             }finally{
                 $this->conn = null;
             }
 
-        }/* função mudar Status da sessão
+        }
+
+        public function calcularTotal($sessao, $qtdIngrInt, $qtdIngrMeia){
+            $sqlPreco = "SELECT id_ingresso FROM sessao WHERE id = $sessao";
+            $stmtPreco = $this->runQuery($sqlPreco);
+            $stmtPreco->execute();
+            $preco = $stmtPreco->fetch(PDO::FETCH_ASSOC);
+            $preco = $preco['id_ingresso'];
+                    
+            $sqlValor = "SELECT valor, meia FROM ingresso WHERE id = $preco";
+            $stmtValor = $this->runQuery($sqlValor);
+            $stmtValor->execute();
+            $valor = $stmtValor->fetch(PDO::FETCH_ASSOC);
+            $valorTotal = ($valor['valor']*$qtdIngrInt)+($valor['meia']*$qtdIngrMeia);
+            return $valorTotal;
+        }
+
+        public function getQtdAssentos($sessao){
+            $sql = "SELECT id_sala FROM sessao WHERE id = $sessao";
+            $stmt = $this->runQuery($sql);
+            $stmt->execute();
+            $idSala = $stmt->fetch(PDO::FETCH_ASSOC);
+            $idSala = $idSala['id_sala'];
+
+            $sqlQtd = "SELECT qtdAssentos FROM sala WHERE id = $idSala";
+            $stmtQtd = $this->runQuery($sqlQtd);
+            $stmtQtd->execute();
+            $qtdAssentos = $stmtQtd->fetch(PDO::FETCH_ASSOC);
+            $qtdAssentos = $qtdAssentos['qtdAssentos'];
+            return $qtdAssentos;
+        }
+
+        public function getIngressosVendidos($sessao){
+            $sql = "SELECT ingressosVendidos FROM sessao WHERE id = $sessao";
+            $stmt = $this->runQuery($sql);
+            $stmt->execute();
+            $ingressosVendidos = $stmt->fetch(PDO::FETCH_ASSOC);
+            $ingressosVendidos = $ingressosVendidos['ingressosVendidos'];
+            return $ingressosVendidos;
+        }
+
+        function getNomeCliente($cpf){
+            $sqlCliente = "SELECT id FROM cliente WHERE cpf = $cpf";
+            $stmtCliente = $this->runQuery($sqlCliente);
+            $stmtCliente->execute();
+            $cliente = $stmtCliente->fetch(PDO::FETCH_ASSOC);
+            return $cliente['id'];
+        }
+        
+        // função mudar Status da sessão
         public function mudarStatus($sessao){
             try{
-                $sqlStatus = "UPDATE sessao SET status ='Vendido' WHERE id =:id";
+                $sqlStatus = "UPDATE sessao SET status ='ENCERRADA' WHERE id =:id";
                 $stmt= $this->conn->prepare($sqlStatus);
                 $stmt->bindParam(":id",$sessao);
                 $stmt->execute();
@@ -78,45 +115,7 @@
             }finally{
                 $this->conn=null;
             }
-        }*/
-        // public function editarSessao($filme,$sala, $data, $horario, $id){
-        //     try{
-        //         $sql = "UPDATE sessao
-        //         SET 
-        //             id_filme = :id_filme,
-        //             id_sala = :id_sala,
-        //             data = :data,
-        //             horarioInicio = :horarioInicio
-        //             WHERE id = :id";
-        //             $stmt = $this->conn->prepare($sql);
-        //             $stmt-> bindParam(":id_filme",$filme);
-        //             $stmt-> bindParam(":id_sala",$sala);
-        //             $stmt-> bindParam(":data",$data);
-        //             $stmt-> bindParam(":horarioInicio",$horario);
-        //             $stmt-> bindParam(":id",$id);
-        //             $stmt->execute();
-             
-        //             return $stmt;
-        //  }catch(PDOException $e){
-        //      echo ("Error: ".$e->getMessage());
-        //  }finally{
-        //      $this->conn = null;
-        //  } 
-        // }
-
-        // public function deletarSessao($idSessao){
-        //     try{
-        //         $sql = "DELETE FROM sessao WHERE id = :id";
-        //         $stmt = $this->conn->prepare($sql);
-        //         $stmt-> bindParam(":id",$idSessao);
-        //         $stmt->execute();
-        //         return $stmt;   
-        //     }catch(PDOException $e){
-        //         echo("Erro: ".$e->getMessage());
-        //     }finally{
-        //         $this->conn=null;
-        //     }
-        // }
+        }
 
          public function redirect($url){
              header("Location: $url"); //header redireciona os links da pagina
